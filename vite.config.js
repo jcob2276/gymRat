9e58ab93e9f24d204d0ae4ee78188926fdd367ca
Jakub <jakubsobon3@gmail.com>
2026-04-28 19:29:30 +0200

    Fix tailwind config and add project files

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       '/oura-api': {
         target: 'https://api.ouraring.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/oura-api/, '')
+        rewrite: (path) => path.replace(/^\/oura-api/, ''),
+        secure: false
       }
     }
   }
